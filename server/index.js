d13b2695f7078e520ea507ead5b446d21c85e332
Anil Suthar <aniesuthar@gmail.com>
2023-09-14 18:20:25 +0530

    THODA SA ROUTES

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+
 import Connection from './database/db.js';
+import Routes from './routes/route.js'
+
 
 const app =  express();
 const PORT = 9000;
+
+
+app.use(cors());
+app.use('/', Routes);
 
 
 
